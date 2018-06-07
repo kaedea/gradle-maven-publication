d6a3503a6cc27538b31aededb6d81ed19152fcf2
Kaede <kidhaibara@gmail.com>
2018-06-07 21:36:55 +0800

    Add 'lib-java-sub' module.

---
+++ lib-java-sub/src/main/java/com/kaedea/demo/gradle/maven/lib/java/LibJavaSub.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018. Kaede
+ */
+
+package com.kaedea.demo.gradle.maven.lib.java;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author Kaede
+ * @since 2017/1/28
+ */
+public class LibJavaSub {
+
+    public static boolean isEmpty(String text) {
+        return StringUtils.isEmpty(text);
+    }
+}
