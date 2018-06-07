52ee4aab1e56ea989e6ffb1c36edc2f747e66543
Kaede <kidhaibara@gmail.com>
2018-06-07 22:17:04 +0800

    Add 'lib-android-sub' module.

---
+++ lib-android-sub/src/main/java/com/kaedea/demo/gradle/maven/lib/android/LibAndroidSub.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018. Kaede
+ */
+
+package com.kaedea.demo.gradle.maven.lib.android;
+
+import com.kaedea.demo.gradle.maven.lib.java.LibJava;
+
+public class LibAndroidSub {
+    public static void foo() {
+        LibJava.foo();
+    }
+}
--- lib-java-sub/src/main/java/com/kaedea/demo/gradle/maven/lib/java/LibJavaSub.java
+++ lib-java-sub/src/main/java/com/kaedea/demo/gradle/maven/lib/java/LibJavaSub.java
@@ -11,8 +11,7 @@ import org.apache.commons.lang3.StringUtils;
  * @since 2017/1/28
  */
 public class LibJavaSub {
-
-    public static boolean isEmpty(String text) {
-        return StringUtils.isEmpty(text);
+    public static void foo() {
+        System.out.println(StringUtils.capitalize("Hello, world!"));
     }
 }
