ee8ad7788eaed618ca4f7d90e56224b07f1322da
Kaede <kidhaibara@gmail.com>
2018-08-06 17:18:58 +0800

    Make testcases work

--- gradle-plugin/publication/src/test/groovy/com/kaedea/gradle/publication/ExtensionTest.groovy
+++ gradle-plugin/publication/src/test/groovy/com/kaedea/gradle/publication/ExtensionTest.groovy
@@ -5,20 +5,29 @@
 package com.kaedea.gradle.publication
 
 import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.contrib.java.lang.system.EnvironmentVariables
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 import static com.kaedea.gradle.publication.Extension.RELEASE_REPOSITORY_URL
 import static com.kaedea.gradle.publication.Extension.SNAPSHOT_REPOSITORY_URL
+import static org.assertj.core.api.Assertions.assertThat
 
+@RunWith(JUnit4.class)
 class ExtensionTest {
 
     private Project project
     private Extension extension
-    private EnvironmentVariables environmentVariables = EnvironmentVariables()
 
-    def opt = { extension.get(it) ?: Utils.readFromProperties(project, it) }
+    @Rule
+    public TestRule environmentVariables = new EnvironmentVariables()
+
+    def opt = { extension.get(it) ?: Utils.readFromProperties(project, it)?.toString() }
     def mock = { "mock.$it" }
     def env = {
         def value = mock("env.$it")
@@ -27,7 +36,7 @@ class ExtensionTest {
     }
     def pro = {
         def value = mock("pro.$it")
-        project.setProperty(it, value)
+        project.extensions.add(it, value)
         return value
     }
 
@@ -40,18 +49,22 @@ class ExtensionTest {
     @Test
     void defaultReleaseRepositoryUrl() {
         assertThat(opt(RELEASE_REPOSITORY_URL)).isNull()
-        def environment = env(RELEASE_REPOSITORY_URL)
+        String environment = env(RELEASE_REPOSITORY_URL)
+        assertThat(environment).isNotBlank()
         assertThat(opt(RELEASE_REPOSITORY_URL)).isEqualTo(environment)
-        def property = pro(RELEASE_REPOSITORY_URL)
+        String property = pro(RELEASE_REPOSITORY_URL)
+        assertThat(property).isNotBlank()
         assertThat(opt(RELEASE_REPOSITORY_URL)).isEqualTo(property)
     }
 
     @Test
     void defaultSnapshotRepositoryUrl() {
         assertThat(opt(SNAPSHOT_REPOSITORY_URL)).isNull()
-        def environment = env(SNAPSHOT_REPOSITORY_URL)
+        String environment = env(SNAPSHOT_REPOSITORY_URL)
+        assertThat(environment).isNotBlank()
         assertThat(opt(SNAPSHOT_REPOSITORY_URL)).isEqualTo(environment)
-        def property = pro(SNAPSHOT_REPOSITORY_URL)
+        String property = pro(SNAPSHOT_REPOSITORY_URL)
+        assertThat(property).isNotBlank()
         assertThat(opt(SNAPSHOT_REPOSITORY_URL)).isEqualTo(property)
     }
 }
